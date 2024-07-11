329536567343d806f9977baa7b8d1710ee19e7d5
apoio-forLEARN <apoio@forlearn.ao>
2024-07-11 16:10:29 +0100

    create page login

--- src/main/frontend/routes.tsx
+++ src/main/frontend/routes.tsx
@@ -3,10 +3,14 @@ import { lazy } from 'react';
 
 const HomeView = lazy(async () => import('Frontend/views/Home.js'));
 const AboutView = lazy(async () => import('Frontend/views/About.js'));
+const LoginView = lazy(async () => import('Frontend/views/Login.js'));
+
+const load = (view: string) => lazy(async () => import(`Frontend/views/${view}.js`));
 
 export const routes = [
     { path: '/', element: <HomeView /> },
     { path: '/about', element: <AboutView /> },
+    { path: '/login', element: <LoginView/> },
 ] as RouteObject[];
 
 export default createBrowserRouter(routes);
